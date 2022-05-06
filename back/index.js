e682ef203e29e2cabd50e2ce59806efe9e77bd46
Turpin Yannick <TurpinYa@gmail.com>
2022-05-06 11:10:00 +0200

    back, commandes principales

--- back/index.js
+++ back/index.js
@@ -5,8 +5,34 @@ const api = express()
 api.use(express.json())
 api.use(cors())
 
-api.get("/annonces", (req, res) =>{
-    res.json("accueil reponse")
+//Endpoint pour créer une annonce
+api.post('/annonces', (req, res) => {
+    const {id, titre, contenu, url} = req.body
+    if(title != undefined && content != undefined) {
+        dataService.createAnnonce(id, titre, contenu,url)
+        res.json({message: "annonce ajoutée"})
+    }
+    else
+        res.json({message: "Merci d'envoyer un titre, un contenu et une photo"})
+})
+
+//Endpoint pour récuperer la liste des annonces
+api.get('/annonces', (req, res) => {
+    res.json(dataService.annonces)
+})
+
+//Endpoint pour récupérer une seule annonce
+api.get('/annonce/:id', (req, res) => {
+    const annonce = dataService.findAnnonceById(req.params.id)
+    if(annonce != undefined)
+        res.json(annonce)
+    else
+        res.json({message: "aucune annonce avec cet id"})
+})
+
+//Endpoint pour récuperer la liste des favoris
+api.get('/favoris', (req, res) => {
+    res.json(dataService.favoris)
 })
 
 api.listen(666,()=>{
