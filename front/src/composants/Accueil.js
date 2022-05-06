4e9bf7de64b6123e9b504ad40c0462024a654f87
gaetan <gs99@hotmail.fr>
2022-05-06 15:54:38 +0200

    modif

--- front/src/composants/Accueil.js
+++ front/src/composants/Accueil.js
@@ -26,36 +26,6 @@ export class Accueil extends Component {
         return ( 
         <div>
             <Recherche/>
-            <br/>
-            {
-                this.state.data==0?
-                <p>Aucune annonce</p>
-                :
-                <Row xs={1} md={3} className="g-4">
-                {this.state.data.map((e,i)=>{
-                    let newContenu = ""
-                    if(e.contenu.length>100){
-                        newContenu = e.contenu.substring(0,100)
-                    }
-                    else{
-                        newContenu = e.contenu
-                    }
-                    return(
-                        <Col key={i}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img className="mx-auto" variant="top" src={e.url} style={{width:150,height:150,marginTop:"10px"}}/>
-                                <Card.Body>
-                                    <Card.Title>{e.titre}</Card.Title>
-                                    <Card.Text>{newContenu}</Card.Text>
-                                    <Link to={"/detail/"+e.id}><img className="buttonAnimeBoy" src="/anime-search.png"/></Link>
-                                    <Link to={"/favoris/"+e.id}><img className="buttonAnimeBoy" src="/anime-heart.png"/></Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    )
-                })}
-                </Row>
-            }
         </div> 
         )
     }
--- front/src/composants/Recherche.js
+++ front/src/composants/Recherche.js
@@ -1,14 +1,39 @@
 import e from "cors";
 import {Component} from "react"
-import { Form } from "react-bootstrap"
+import {Link} from "react-router-dom"
+import { Form,Card,Row,Col } from "react-bootstrap"
 const axios = require('axios')
 
 export class Recherche extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            titre: undefined
+            titre: undefined,
+            data: []
         }
+        axios.get('http://localhost:666/annonces')
+            .then((response) => {
+                let arrayAnnonces = []
+                for(const element of response.data){
+                    if(""==element.titre){
+                        arrayAnnonces.push(element)
+                    }
+                }
+                if(arrayAnnonces==0){
+                    arrayAnnonces = response.data
+                }
+                this.setState({data:arrayAnnonces})
+                console.log(arrayAnnonces)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+    ajouterFavori(e,id){
+        e.preventDefault()
+
+        document.getElementById("button"+id).style.visibility = "hidden"
+        console.log(document.getElementById("button"+id))
     }
     rechercher(e){
         e.preventDefault()
@@ -23,6 +48,7 @@ export class Recherche extends Component {
                 if(arrayAnnonces==0){
                     arrayAnnonces = response.data
                 }
+                this.setState({data:arrayAnnonces})
                 console.log(arrayAnnonces)
             })
             .catch(function (error) {
@@ -31,7 +57,38 @@ export class Recherche extends Component {
     }
     render() { 
         return ( 
+            <div>
             <Form.Control type="text" placeholder="Taper le titre d'une annonce" onChange={(e)=>this.rechercher(e)}/>
+            {
+                this.state.data==0?
+                <p>Aucune annonce</p>
+                :
+                <Row xs={1} md={3} className="g-4">
+                {this.state.data.map((e,i)=>{
+                    let newContenu = ""
+                    if(e.contenu.length>100){
+                        newContenu = e.contenu.substring(0,100)
+                    }
+                    else{
+                        newContenu = e.contenu
+                    }
+                    return(
+                        <Col key={i}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img className="mx-auto" variant="top" src={e.url} style={{width:150,height:150,marginTop:"10px"}}/>
+                                <Card.Body>
+                                    <Card.Title>{e.titre}</Card.Title>
+                                    <Card.Text>{newContenu}</Card.Text>
+                                    <Link to={"/detail/"+e.id}><img className="buttonAnimeBoy" src="/anime-search.png"/></Link>
+                                    <button style={{borderRadius:"100px",border:"white"}} id={"button"+e.id} onClick={(y)=>this.ajouterFavori(y,e.id)}><img className="buttonAnimeBoy" src="/anime-heart.png"/></button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+                </Row>
+            }
+            </div>
         )
     }
 }
