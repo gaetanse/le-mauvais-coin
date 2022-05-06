5134bc9b2ce4c1fc69d4ff315857073c1c2ef961
Turpin Yannick <TurpinYa@gmail.com>
2022-05-06 14:08:08 +0200

    front, favori

--- front/src/composants/Favoris.js
+++ front/src/composants/Favoris.js
@@ -1,15 +1,52 @@
 import { Component } from "react"
+import { Button,Card,CardGroup,Row,Col } from "react-bootstrap"
+
+const axios = require('axios')
 
 export class Favoris extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: []
+        }
     }
-    state = {  }
+    componentDidMount(){
+        axios.get('http://localhost:666/favoris')
+            .then((response) => {
+                //console.log(response.data);
+                this.setState({data:response.data})
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
     render() { 
         return ( 
-            <div>
-                favoris
-            </div>
+        <div>
+            <br/>
+            {
+                this.state.data===0?
+                <p>Pas de Favori</p>
+                :
+                <Row xs={1} md={3} className="g-4">
+                {this.state.data.map((e,i)=>{
+                    return(
+                        <Col key={i}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img className="mx-auto" variant="top" src={e.url} style={{width:150,height:150,marginTop:"10px"}}/>
+                                <Card.Body>
+                                    <Card.Title>{e.titre}</Card.Title>
+                                    <Card.Text>{e.contenu}</Card.Text>
+                                
+                                    <img className="buttonAnimeBoy" src="/anime-heart.png"/>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+                </Row>
+            }
+        </div> 
         )
     }
 }
